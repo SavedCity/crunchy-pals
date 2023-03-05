0d9df9200cfa6f0521be59903b88b9d55ffd3dcc
Andy Checo <andycheco25@gmail.com>
2023-03-05 10:15:43 -0500

    my reviews endpoint

---
+++ src/pages/api/reviews/my-reviews/[slug].ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import Reviews from 'utils/schema/Review'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    const { slug } = req.query
+
+    try {
+      const reviews = await Reviews.find({ createdBy: slug })
+      return res.json({ status: 200, reviews })
+    } catch (error) {
+      return res.json({ status: 400, error: 'Bad request' })
+    }
+  }
+
+  return res.json({
+    status: 400,
+    error: 'This API call only accepts GET methods',
+  })
+}
